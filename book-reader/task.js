8640c9b9c258f9b87f4b2be55efc3af0fefbdbf4
Lonskoy <lonskoy0304@gmail.com>
2022-12-17 22:00:09 +0300

    edit

--- book-reader/task.js
+++ book-reader/task.js
@@ -1,26 +1,22 @@
 const arrFont = Array.from(document.getElementsByClassName('font-size'));
-const book = document.getElementsByClassName('book__content');
+const book = document.querySelector('.book__content');
 
 
 
 arrFont.forEach(elem => {
-    elem.addEventListener('event', (event) => {
-        event.preventDefault();
-    });
-
-    elem.addEventListener('click', () => {
-    if(!elem.classList.contains('font-size_active')) {         //Элемент НЕ содержит класс font-size_active
-        console.log('не содержит');
-        for(let i = 0; i < arrFont.length; i++) {             //Находим и удаляем класс font-size_active у другого элемента
-            if (arrFont[i].classList.contains('font-size_active')) {
-                arrFont[i].classList.remove('font-size_active');
+    elem.addEventListener('click', (event) => {
+        event.preventDefault();    
+        if(!elem.classList.contains('font-size_active')) {         //Элемент НЕ содержит класс font-size_active
+            for(let i = 0; i < arrFont.length; i++) {             //Находим и удаляем класс font-size_active у другого элемента
+                if (arrFont[i].classList.contains('font-size_active')) {
+                    arrFont[i].classList.remove('font-size_active');
+                }
             }
-        }
-        elem.classList.add('font-size_active');               //Присваеваем класс font-size_active элементу на который кликнули                                   //Запрещаем переход по ссылке ???НЕ РАБОТАЕТ???
-    } 
-    else {
-        console.log('содержит');
-    }
+            elem.classList.add('font-size_active');               //Присваеваем класс font-size_active элементу на который кликнули 
+            let temp = elem.getAttribute('data-size');          // Устанавливаем шрифт текста
+            console.log(temp, book.className ='book__content font-size_' + temp);
+        } 
+
     });
 });
 
